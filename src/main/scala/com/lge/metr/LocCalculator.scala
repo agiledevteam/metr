6853f678790bcaba3fd41618723f195de16ac78c
Jooyung Han <jooyung.han@gmail.com>
2013-12-11 18:14:29 +0900

    Print plain vs dloc

--- src/main/scala/com/lge/metr/LocCalculator.scala
+++ src/main/scala/com/lge/metr/LocCalculator.scala
@@ -1,15 +1,14 @@
 package com.lge.metr
 
 import scala.collection.JavaConversions.asScalaSet
-
 import spoon.processing.AbstractProcessor
 import spoon.reflect.declaration.CtClass
+import spoon.reflect.code.CtBlock
 
 class LocCalculator extends AbstractProcessor[CtClass[_]] with Naming with LocCounter {
-
   override def process(klass: CtClass[_]) {
     for (c <- klass.getAllMethods ++ klass.getConstructors if !c.isImplicit()) {
-      println(nameFor(c) + ":" + plainLoc(c.getBody.toString))
+      println(nameFor(c) + "\t" + plainLoc(c.getBody.toString) + "\t" + dloc(c.getBody))
     }
   }
 }
