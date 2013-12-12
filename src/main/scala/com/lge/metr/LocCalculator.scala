a430442eac96b5828e0c13b1501b2cde7fccf869
Jooyung Han <jooyung.han@gmail.com>
2013-12-12 15:28:43 +0900

    Fix compile error

--- src/main/scala/com/lge/metr/LocCalculator.scala
+++ src/main/scala/com/lge/metr/LocCalculator.scala
@@ -8,7 +8,6 @@ import spoon.reflect.code.CtBlock
 class LocCalculator extends AbstractProcessor[CtClass[_]] with Naming with LocCounter {
   override def process(klass: CtClass[_]) {
     for (c <- klass.getAllMethods ++ klass.getConstructors if !c.isImplicit() && c.getBody != null) {
-    for (c <- klass.getAllMethods ++ klass.getConstructors if !c.isImplicit()) {
       println(nameFor(c) + "\t" + plainLoc(c.getBody.toString) + "\t" + dloc(c.getBody))
     }
   }
